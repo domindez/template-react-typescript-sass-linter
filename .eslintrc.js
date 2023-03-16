e5e0aae768378b8d15477ac5e5fcd687a0bdd275
domindez <109239601+domindez@users.noreply.github.com>
2023-03-16 17:14:11 +0100

    tabs

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
     'jsx-quotes': ['error', 'prefer-single'],
-    'react/react-in-jsx-scope': 'off'
+    'react/react-in-jsx-scope': 'off',
+    'no-tabs': 'off'
   }
 }
